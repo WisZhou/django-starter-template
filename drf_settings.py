dedab00d1d374ea947ff547dcaa3a01374b69374
runforever <c.chenchao.c@gmail.com>
2016-12-07 18:26:10 +0800

    add docker settings

--- drf_settings.py
+++ drf_settings.py
@@ -4,7 +4,6 @@ import datetime
 INSTALLED_APPS += [
     'rest_framework',
     'rest_framework_jwt',
-    'rest_framework_swagger',
     'corsheaders',
 ]
 
---
+++ fabfile.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+
+from fabric.api import (
+    local,
+)
+
+
+def runserver(port=7070):
+    '''
+    start dev django server
+    '''
+    local(
+        'docker-compose run --rm -p {port}:{port} web '
+        'python manage.py runserver 0.0.0.0:{port}'.format(port=port)
+    )
+
+
+def manage(cmd):
+    '''
+    run django manage command
+    '''
+    local('docker-compose run --rm web python manage.py {cmd}'.format(cmd=cmd))
+
+
+def makemigrations(merge=None):
+    '''
+    database migrate
+    '''
+    cmd = 'makemigrations' if merge is None else 'makemigrations --merge'
+    manage(cmd)
+
+
+def migrate(version=None):
+    '''
+    database migrate
+    '''
+    migrate_cmd = 'migrate hitravel {0}'.format(version) if version \
+        else 'migrate'
+    manage(migrate_cmd)
+
+
+def shell():
+    '''
+    shell
+    '''
+    manage('shell')
+
+
+def create_app(name):
+    '''
+    create django app
+    '''
+    local(
+        'docker-compose run --rm web django-admin.py '
+        'startapp {name}'.format(name=name)
+    )
+
+
+def clean():
+    '''
+    clean pyc
+    '''
+    local("find . -name '*.pyc' -type f -print -exec rm -rf {} \;")
